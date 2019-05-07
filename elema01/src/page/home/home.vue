5c0dbdcdcf894c1b226d578dafb24940498d3643
PC-20180906AQCF\Administrator <1477264946@qq.com>
2019-05-07 17:41:28 +0800

    1

--- elema01/src/page/home/home.vue
+++ elema01/src/page/home/home.vue
@@ -1,19 +1,45 @@
 <template>
-    <zhead>
-      <span slot="logo">ele.me</span>
-    </zhead>
+    <div class="city_title">
+        <div class="table">
+            <span>当前定位城市</span>
+            <span>定位不准时,请在城市列表中选择</span>
+        </div>
+        <a href="#" class="">
+            <span></span>
+            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+        </a>
+    </div>
 </template>
 
 <script>
-  import zhead from '../../components/header/head'
+    import Vue from 'vue';
     export default {
         name: "home",
-      components:{
-          zhead
-      }
+        data(){
+            return {
+                allCity:[],
+            }
+        },
+        mounted(){
+            //  当组件挂载完毕时发起网络请求
+            Vue.axios.get('https://elm.cangdu.org/v1/cities?type=group').then((res)=>{
+                console.log(res.data);
+                this.allCity=res.data;
+            }).catch((error)=>{
+                console.log('请求错误:' ,error);
+            });
+
+        },
     }
 </script>
 
 <style scoped>
-
+    .city_title{
+        color: #666;
+        font-weight: 400;
+        text-indent: .45rem;
+        border-top: 2px solid #e4e4e4;
+        border-bottom: 1px solid #e4e4e4;
+        font: .55rem/1.45rem Helvetica Neue;
+    }
 </style>
